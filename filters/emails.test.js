27953dbee62c06e7b60dede41dabd93189b5f837
murilo silva <murilo.silva@WINTERMUTE.local>
2020-11-28 20:55:03 -0300

    Updating filters/emails test suite.

--- filters/emails.test.js
+++ filters/emails.test.js
@@ -1,3 +1,15 @@
-test('testing the test', () => {
-  expect((1 + 2)).toBe(3);
+const filterEmails = require('./emails');
+
+const mock = {
+  rawText: 'aqui dentro dessa string eu tenho os emails wix@hackers.net e também tenho mac@hackers.net.',
+  terms: 'hackers.net',
+};
+const filteredEmails = filterEmails(mock.rawText, mock.terms);
+
+test('Testing the response type', () => {
+  expect(Array.isArray(filteredEmails)).toBeTruthy()
+})
+
+test('Testing the response data', () => {
+  expect(filteredEmails).toStrictEqual(["wix@hackers.net", "mac@hackers.net"]);
 });
