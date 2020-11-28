c4ff4c020096a63bf06409a83cb24683bc5957c3
Wixmix <vitor.borges@computacao.ufla.br>
2020-11-28 17:28:40 -0300

    Subindo testes com o novo metodo de busca

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ const app = express();
 const port = 666;
 
 function filterEmails(rawText, terms) {
-    return rawText.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+)/gi).reduce((uniq, item) => {
+    return rawText.match(/(3*[a-zA-Z0-9]+[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+)/gi).reduce((uniq, item) => {
         return uniq.includes(item) ? uniq : [...uniq, item]
     }, []).filter((email) => email.includes(terms));
 }
 
-app.get('/', (req, res) => {
+app.get('/google', (req, res) => {
     const { terms, page } = req.query;
-    axios.get(`https://www.google.com.br/search?num=100&q=%40"${terms}"&start=${page * 10}`).then((a) => {
+    axios.post('https://www.startpage.com/sp/search', {
+        data: `query=%40%22${terms}%22&language=english&lui=english&cat=web&sc=vsucL4DHGoY810&abp=${page}`,
+        timeout: 0,
+        headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0',
+            'Referrer': 'https://www.startpage.com/',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    }).then((a) => {
+        console.log('entrei garaio');
         const $ = cheerio.load(a.data);
-        const rawText = $('.BNeawe').find('div').text();
-        res.send(filterEmails(rawText, terms));
+        const rawText = $('.w-gl__result').find('p').text();
+        console.log();
+        res.send(a.data);
     })
     .catch((e) => {
-        res.send('deu ruim');
+        res.send(e);
     });
 });
 
